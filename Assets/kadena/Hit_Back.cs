693e7955565d44f182e1e54fc262751bcf78eff2
LAPTOP-ERHGB55S\k.marbow <masatoshi-k-17e@stu.kbc.ac.jp>
2019-01-30 17:59:30 +0900

    //追加・修正 PlayerController.csにてプレイヤーの挙動修正　当たり判定用の4つのHit.cs追加

---
+++ Assets/kadena/Hit_Back.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit_Back : MonoBehaviour {
+
+    public bool Back_col = false;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Back_col = true;
+    }
+}
---
+++ Assets/kadena/Hit_Left.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hit_Left : MonoBehaviour {
+
+    public bool Left_col = false;
+    Rigidbody rb;
+    void OnTriggerEnter(Collider other)
+    {
+        
+        Left_col = true;
+    }
+}
