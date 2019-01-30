c867241bcad50934ab74e43e354cbe8e5274eac7
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-01-30 18:27:37 +0900

    // 結合ファイル名の変更、通常敵が同じレーンに連続生成されないように変更。

--- Assets/wakugawa/Scripts/NormalCarSponer.cs
+++ Assets/wakugawa/Scripts/NormalCarSponer.cs
@@ -6,26 +6,35 @@ public class NormalCarSponer : MonoBehaviour
 {
 
     public GameObject[] EnemyCar;
-
     public GameObject[] SpawnPoint;
 
     float SpawnTime;
+    int OldSpawnPoint;      // 前回の生成場所を保存する変数
 
     // Use this for initialization
     void Start()
     {
+        OldSpawnPoint = 99;
         SpawnTime = Random.Range(0.2f, 0.8f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        int NowSpawnPoint;  // 現在の生成場所を保存する変数
         SpawnTime -= Time.deltaTime;
 
         if (SpawnTime < 0)
         {
-            Instantiate(EnemyCar[Random.Range(0, EnemyCar.Length - 1)], SpawnPoint[Random.Range(0, SpawnPoint.Length - 1)].transform);
+            NowSpawnPoint = Random.Range(0, SpawnPoint.Length - 1);
+
+            while (NowSpawnPoint == OldSpawnPoint)
+            {
+                NowSpawnPoint = Random.Range(0, SpawnPoint.Length - 1);
+            }
+
+            OldSpawnPoint = NowSpawnPoint;
+            Instantiate(EnemyCar[Random.Range(0, EnemyCar.Length - 1)], SpawnPoint[NowSpawnPoint].transform);
             SpawnTime = Random.Range(0.2f, 0.8f);
         }
     }
--- Assets/wakugawa/Scripts/PoliceCarSponer.cs
+++ Assets/wakugawa/Scripts/PoliceCarSponer.cs
@@ -6,6 +6,8 @@ public class PoliceCarSponer : MonoBehaviour {
 
     public GameObject PoliceCar;
 
+    float time = 0;
+
     // Use this for initialization
     void Start () {
 		
@@ -13,6 +15,11 @@ public class PoliceCarSponer : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		
+        time += Time.deltaTime;
+        if(time > 15)
+        {
+            time = 0;
+            //Instantiate(PoliceCar,)
+        }
 	}
 }
