d414503a668dce73ddfd60504bd708aaecc11698
Chalice <tatsuki-k-17e@stu.kbc.ac.jp>
2019-01-31 19:44:08 +0900

    [追加]Titleシーンの追加、それに関する様々物を作成・追加

---
+++ Assets/kaneshi/Scripts/MoveScene.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MoveScene : MonoBehaviour
+{
+    public float WaitTime = 7.0f;
+    public string SceneName = "";
+    private float nowTime;
+    private bool Startflg = false;
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	    if(Input.GetButtonDown("Horizontal"))
+        {
+            Startflg = true;
+        }
+        if (!Startflg) { return; }
+
+        nowTime += Time.deltaTime;
+
+        if(nowTime >= WaitTime)
+        {
+            SceneManager.LoadScene(SceneName);
+        }
+	}
+}
---
+++ Assets/kaneshi/Scripts/TitlePressTextController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TitlePressTextController : MonoBehaviour
+{
+    private float multipliedAlpha = 1;
+    private Text myText;
+    private Color nowColor;
+    private bool Startflg = false;
+    private int Waitflame;
+
+	// Use this for initialization
+	void Start ()
+    {
+        myText = GetComponent<Text>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if(Input.GetButton("Horizontal"))
+        {
+            Startflg = true;
+        }
+
+        if (!Startflg)
+        {
+            nowColor = myText.color;
+            myText.color = new Color(nowColor.r, nowColor.g, nowColor.b,
+                                        nowColor.a - (1.0f * Time.deltaTime * multipliedAlpha));
+            if (myText.color.a < 0 || myText.color.a > 1)
+            {
+                multipliedAlpha *= -1;
+            }
+        }
+        else
+        {
+            if(++Waitflame > 2)
+            {
+                myText.color = new Color(nowColor.r, nowColor.g, nowColor.b,
+                                        nowColor.a * multipliedAlpha);
+                multipliedAlpha *= -1;
+                Waitflame = 0;
+            }
+        }
+    }
+}
