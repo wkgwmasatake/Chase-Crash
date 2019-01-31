3943fccc8b823dc84eb3d88fe0561dde13197a5c
LAPTOP-ERHGB55S\k.marbow <masatoshi-k-17e@stu.kbc.ac.jp>
2019-01-31 17:18:12 +0900

    Merge branch 'master' of https://github.com/wkgwmasatake/Chase-Crash

--- Assets/wakugawa/Scripts/PoliceCarSponer.cs
+++ Assets/wakugawa/Scripts/PoliceCarSponer.cs
@@ -7,6 +7,8 @@ public class PoliceCarSponer : MonoBehaviour {
     public GameObject PoliceCar;
     public GameObject[] SpawnPoint;
 
+    public PlayerController PlayerInfo;
+
     float time = 0;
 
     // Use this for initialization
@@ -17,8 +19,26 @@ public class PoliceCarSponer : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
         time += Time.deltaTime;
-        if(time > 15)               // 一定時間経過後に生成(結合後に変更)
+        if(time > 5)               // 一定時間経過後に生成(結合後に変更)
         {
+            Transform PlayerTransform = PlayerInfo.GetComponent<PlayerController>().ReturnPlayerTransform();
+
+            if ((PlayerTransform.position.x + 8) / 4 == 0)     // レーンの左端にいたら
+            {
+                Instantiate(PoliceCar, SpawnPoint[(((int)PlayerTransform.position.x + 8) / 4) + 1].transform);      // プレイヤーの右側に生成
+            }
+            else if((PlayerTransform.position.x + 8) / 4 == 4)  // レーンの右端にいたら
+            {
+                Instantiate(PoliceCar, SpawnPoint[(((int)PlayerTransform.position.x + 8) / 4) - 1].transform);      // プレイヤーの左側に生成
+            }
+            else if (Random.Range(-1.0f, 1.0f) < 0)             // ランダム値が0未満なら
+            {
+                Instantiate(PoliceCar, SpawnPoint[(((int)PlayerTransform.position.x + 8) / 4) + 1].transform);      // プレイヤーの右側に生成
+            }
+            else                                                // ランダム値が0以上なら
+            {
+                Instantiate(PoliceCar, SpawnPoint[(((int)PlayerTransform.position.x + 8) / 4) - 1].transform);      // プレイヤーの左側に生成
+            }
             time = 0;
             //Instantiate(PoliceCar,)
         }
