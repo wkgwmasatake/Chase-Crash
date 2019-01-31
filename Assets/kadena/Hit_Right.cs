c11e4be569b6b44125d44927c43c73460685d546
LAPTOP-ERHGB55S\k.marbow <masatoshi-k-17e@stu.kbc.ac.jp>
2019-01-31 17:05:25 +0900

    //修正　プレイヤーと敵が左右どちらかに衝突する時の処理を修正 プレイヤーのレーンをget出力できる処理を追加

--- Assets/kadena/Hit_Right.cs
+++ Assets/kadena/Hit_Right.cs
@@ -5,14 +5,17 @@ using UnityEngine;
 public class Hit_Right : MonoBehaviour {
 
     public bool Right_col = false;
-    public bool right_enemy;
+    public bool right_enemy;//敵と接触したか否か
     public GameObject parent;
-
+    public GameObject player;
+    PlayerController player_check;
     void Start()
     {
         parent = transform.parent.gameObject;
         right_enemy = false;
+        player_check = player.GetComponent<PlayerController>();
     }
+
     void OnTriggerEnter(Collider other)
     {
         Right_col = true;
@@ -30,7 +33,17 @@ public class Hit_Right : MonoBehaviour {
         else if(other.gameObject.tag == "Enemy")//特殊敵と衝突
         {
             Debug.Log("NormalEnemy");
-            right_enemy = true;
+            if(player_check.player_move == false)
+            {
+                right_enemy = true;
+                
+            }
+            else
+            {
+                Debug.Log("Blast!!");
+                other.transform.position = Vector3.MoveTowards(other.transform.position, new Vector3(other.transform.position.x + 4, 0, 0), 200 * Time.deltaTime);
+            }
+
         }
     }
 }
