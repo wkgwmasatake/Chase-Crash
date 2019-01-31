bb21b1d0ee38fa1e2de0aa50a34f159db1fff4a1
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-01-31 22:09:43 +0900

    // SceneMainの結合 // 破壊数、スピードメーターのUI(仮)の追加

---
+++ Assets/wakugawa/Scripts/SpeedScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpeedScript : MonoBehaviour {
+
+    Text SpeedText;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Text>().text = GameStateStash._maxSpeed.ToString();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
