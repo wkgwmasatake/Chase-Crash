cd205bb240213c9b80435606315037e2af99cdf9
LAPTOP-ERHGB55S\k.marbow <masatoshi-k-17e@stu.kbc.ac.jp>
2019-01-31 19:55:14 +0900

    //追加 プレイヤーの外見をCubeからCar2に変更しました

--- Assets/kadena/Hit_Front.cs
+++ Assets/kadena/Hit_Front.cs
@@ -5,15 +5,19 @@ using UnityEngine;
 public class Hit_Front : MonoBehaviour {
 
 	public bool Front_col = false;
+    public GameObject parent;
 
+    void Start()
+    {
+        parent = transform.parent.gameObject;
+    }
     void OnTriggerEnter(Collider other)
     {
         Front_col = true;
-        if (other.gameObject.tag == "Enemy_1")//
+        if (other.gameObject.tag == "NormalEnemy")
         {
-            Debug.Log("Unttaged");
-
+            Debug.Log("NormalEnemy");
+            Destroy(parent.gameObject);
         }
-        
     }
 }
