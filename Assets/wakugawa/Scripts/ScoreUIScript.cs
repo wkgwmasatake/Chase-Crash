f85fef9169e516e71846007b708529310e6fe4d1
DESKTOP-SBQ2E7T\masatake <mstkwkgw@gmail.com>
2019-02-01 12:01:51 +0900

    // ScoreUIScriptsの追加

---
+++ Assets/wakugawa/Scripts/ScoreUIScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreUIScript : MonoBehaviour {
+
+    int OldScore;
+
+	// Use this for initialization
+	void Start () {
+        this.GetComponent<Text>().text = GameStateStash._score.ToString();
+        OldScore = (int)GameStateStash._score;
+    }
+
+    // Update is called once per frame
+    void Update () {
+		if(OldScore != GameStateStash._score)
+        {
+            this.GetComponent<Text>().text = GameStateStash._score.ToString();
+            OldScore = (int)GameStateStash._score;
+        }
+	}
+}
